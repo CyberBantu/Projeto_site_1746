a9fdc93901645008fd21feb9c63b669b2b56b493
Christian Basilio <Christianbasilio97@gmail.com>
2023-05-01 12:16:05 -0300

    Adicional

---
+++ dash.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import basedosdados as bd
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+# importando a bse
+df = pd.read_csv('ocor_1746_31abril.csv', sep = ';')
+
+
+
+bairros = list(df['nome'].unique())
+bairro = st.sidebar.selectbox('Bairro', bairros) # Baixa  de seleção no menu lateral usando duas funções
+
+colunas = 'id_chamado'
+column = st.sidebar.selectbox('Chamados', colunas)
+
+# Grafico
+fig = px.line(df, x = 'data_inicio', y = 'id_chamado', title = column + ' - ' + bairro )
+fig.update_layout( xaxis_title = 'Data', yaxis_title = column.upper(), title = {'x':0.5})
+
+
+# Colocando um titulo
+st.title('Ocorrências no 1746 por Bairros na Cidade do Rio de Janeiro')
+
+# Texto menor e normal
+st.write('O Grafico Abaixo fala sobre Ocorrências no 1746')
+
+st.caption('Produzido por Christian Basilio')
+
+st.plotly_chart(fig, use_contrainer_width = True)
+
+fig.show()
